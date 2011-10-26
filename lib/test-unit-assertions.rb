d8560dd0abc4af5a8510675b557784605ab1a747
Peter Suschlik <ps@neopoly.de>
2011-10-26 17:23:09 +0200

    Initial commit

---
+++ lib/test-unit-assertions.rb
@@ -0,0 +1,9 @@
+require "test-unit-assertions/version"
+
+module Test
+  module Unit
+    module Assertions
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/test-unit-assertions/version.rb
@@ -0,0 +1,7 @@
+module Test
+  module Unit
+    module Assertions
+      VERSION = "0.0.1"
+    end
+  end
+end
