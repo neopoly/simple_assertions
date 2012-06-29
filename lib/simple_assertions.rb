cbc110c9b04446c9fc17475cd17f5d817bf7be29
Peter Suschlik <ps@neopoly.de>
2012-06-29 15:44:37 +0200

    Enhance assert_raises with attribute matcher.

--- lib/simple_assertions.rb
+++ lib/simple_assertions.rb
@@ -1,5 +1,6 @@
 module SimpleAssertions
   autoload :AssertErrorsOn, 'simple_assertions/assert_errors_on'
+  autoload :AssertRaises,   'simple_assertions/assert_raises'
 
   autoload :VERSION,        'simple_assertions/version'
 end
---
+++ lib/simple_assertions/assert_raises.rb
@@ -0,0 +1,29 @@
+module SimpleAssertions
+  # Adds attribute matcher to +assert_raises+.
+  #
+  # == Example
+  #
+  #   # old behaviour
+  #   assert_raises(StandardError) { ... }
+  #
+  #   # exact match on attribute message
+  #   assert_raises(RuntimeError, :message => "yay!") do
+  #     raise "yay!"
+  #   end
+  #
+  #   # pattern match on attributes
+  #   assert_raises(MyError, :message => /foo/, :code => 23) do
+  #     raise MyError, "foo bar", 23
+  #   end
+  module AssertRaises
+    def assert_raises(*args, &block)
+      attributes = args.last.is_a?(Hash) ? args.pop : {}
+      exception = super(*args, &block).tap do |exception|
+        attributes.each do |attribute, expected|
+          actual = expected.send(attribute)
+          assert_operator expected, :===, actual
+        end
+      end
+    end
+  end
+end
